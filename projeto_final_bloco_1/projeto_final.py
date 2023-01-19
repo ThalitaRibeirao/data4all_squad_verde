9860c8f0b0c9283f4e1b3282d7a727a3e986deb2
ThalitaRibeirao <thaly.alvessilva@gmail.com>
2023-01-19 20:29:28 -0300

    adding files

---
+++ projeto_final_bloco_1/projeto_final.py
@@ -0,0 +1,48 @@
+menu = '''
+\nBoas vindas ao nosso sistema:
+
+1 - Inserir usuário
+2 - Excluir usuário
+3 - Atualizar usuário
+4 - Informações de um usuário
+5 - Informações de todos os usuários
+6 - Sair\n
+'''
+
+sub_menu = '''
+\nQual informação deseja alterar?
+1 - Nome
+2 - Tefone
+3 - Endereço\n
+'''
+
+users = [
+    [1,"Ciclano Sauro",2345678,"Rua Boa",True],
+    [2,"Fulano Silva",3478358235,"Rua Top",True],
+    [3,"John Doe",3853385267,"MAIOBÃO",True],
+    [4, "Aline Carvalho", 32988427689, "Rua Roberto Thomazeli", True],
+    [5, "Felipe Cunha", 81998057401, "Rua General Polidoro", True],
+    [6, "Eduardo Vitor", 12988723166, "Rua Maria Pereira", True],
+    [7, "Thalita Silva", 11937523634, "Rua Domingos Maciel", True]
+]
+
+
+while(True): 
+    print(menu)
+    opcao = input('> ')
+    
+    if(opcao=='1'):
+        pass
+    elif(opcao=='2'):
+        pass
+    elif(opcao=='3'):
+        pass
+    elif(opcao=='4'):
+        pass
+    elif(opcao=='5'):
+        pass
+    elif (opcao=='6'): break
+    else:
+        print("Digite uma opção válida!")
+
+        
